dbc09e85f8e027ab619db2f04120b4a1bdfe2fe3
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-08 18:12:18 +0500

    Task3

---
+++ Tyuiu.KorolkovDS.Sprint4.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KorolkovDS.Sprint4.Task3.V5.Lib
+{
+    public class DataService : ISprint4Task3V5
+    {
+        public int Calculate(int[,] array)
+        {
+            int res = 0;
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0;  j < columns - 1; j++)
+                {
+                    if (j ==1)
+                        res += array[i, j];
+                }
+
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint4.Task3.V5/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task3.V5.Lib;
+DataService ds = new DataService();
+int[,] array = new int[5, 5] { { 3, 4, 8, 6, 3 },
+                               { 9, 9, 3, 6, 9 }, 
+                               { 5, 6, 7, 2, 3 }, 
+                               { 3, 6, 2, 3, 8 }, 
+                               { 5, 7, 2, 6, 4 } };
+
+int rows = array.GetUpperBound(0) + 1;
+int columns = array.Length / rows;
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив: ");
+
+for  (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"{array[i, j]} \t");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Сумма элементов первого столбца " + ds.Calculate(array));
+Console.ReadKey();
