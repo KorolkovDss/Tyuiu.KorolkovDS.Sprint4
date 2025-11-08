4cf335b9e67ba48a159030902bb9f7c3c3a2f044
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-08 22:06:08 +0500

    Task6

---
+++ Tyuiu.KorolkovDS.Sprint4.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task6.V1.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint4.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] array = { "Яблоко", "Банан", "Вишня", "Драгонфрут", "Бузина", "Инжир", "Виноград" };
+            int res = ds.Calculate(array);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint4.Task6.V1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task6.V1.Lib;
+DataService ds  = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+var array = new string[] { "Яблоко", "Банан", "Вишня", "Драгонфрут", "Бузина", "Инжир", "Виноград" };
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Количество элементов длина которых > 6");
+int res = ds.Calculate(array);
+Console.WriteLine(res);
+Console.ReadKey();
