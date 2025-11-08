32b9146690aa3856c40e962f57faaec4e1505372
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-08 22:36:47 +0500

    Task7

---
+++ Tyuiu.KorolkovDS.Sprint4.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task7.V15.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint4.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "10293847";
+            int n = 4;
+            int m = 2;
+            int wait = 4;
+            int res = ds.Calculate(n, m, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+    
+}
---
+++ Tyuiu.KorolkovDS.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task7.V15.Lib;
+DataService ds =  new DataService();
+int n = 4;
+int m = 2;
+int[,] matrix = new int[n, m];
+string value = "10293847";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+int index = 0;
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.WriteLine($"{value[i]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(n, m, value);
+Console.WriteLine("Количество нечетных элементов массива = " + res);
+Console.ReadKey();
