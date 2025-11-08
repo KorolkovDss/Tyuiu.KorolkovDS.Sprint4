7de30d07ef7358d8b13e96463759be6486fa117b
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-08 18:53:45 +0500

    Task4

---
+++ Tyuiu.KorolkovDS.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KorolkovDS.Sprint4.Task4.V30.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[,] { { 3, 4, 1 },
+                                         { 7, 2, 3 },
+                                         { 9, 4, 2 } } ;
+
+            
+
+            int[,] wait = new int[,] { {0, 4, 0},
+                                       {0, 2, 0},
+                                       {0, 4, 2} };
+            int[,] res = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using Tyuiu.KorolkovDS.Sprint4.Task4.V30.Lib;
+DataService ds  = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+Console.WriteLine("***************************************************************************");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"Введите {i},{j} элемент массива: ");
+        matrix[i,j]= Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine("\nМассив: ");
+for (int i = 0;i < rows; i++)
+{
+    for (int j = 0;j < columns; j++)
+    {
+        Console.WriteLine($"{matrix[i, j]} \t");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int[,] res = ds.Calculate(matrix);
+Console.WriteLine("Массив без нечетных элементов:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(res[i, j] + "\t");
+    }
+}
+    Console.ReadKey();
